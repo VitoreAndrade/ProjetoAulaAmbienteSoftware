a6fb80fc8c053f3662724333beb75cdaa216499a
Fernandespinheiro <franciscofernandes@unifor.br>
2023-11-27 22:23:38 -0300

    criação da classe professor trabalho finalizado

--- src/br/com/vitor/Main.java
+++ src/br/com/vitor/Main.java
@@ -2,11 +2,12 @@ package br.com.vitor;
 
 import br.com.vitor.model.Aluno;
 import br.com.vitor.model.Disciplinas;
+import br.com.vitor.model.Professor;
 
 
 public class Main {
     public static void main(String[] args) {
-
+        Professor professorMatematica = new Professor("Doutor Silva", "Matemática");
         Aluno aluno1 = new Aluno("João", 1);
         Aluno aluno2 = new Aluno("Maria", 2);
 
@@ -19,5 +20,10 @@ public class Main {
         Disciplinas matematica = new Disciplinas("Matemática",professorMatematica);
         matematica.adicionarAluno(aluno1);
         matematica.adicionarAluno(aluno2);
+
+        professorMatematica.adicionarDisciplina(matematica);
+
+        matematica.exibirInformacoesProfessor();
+        matematica.exibirMediaTurma();
     }
 }
